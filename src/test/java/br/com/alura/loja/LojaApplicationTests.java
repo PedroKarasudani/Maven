e1dc6e56564f0cbf3a5da1c55572002ca0389b3b
Pedro K. L <pedro.karasudani@hotmail.com>
2023-07-31 22:04:31 -0300

    Adicionando uma nova dependência
    Dependência pesquisada no Google

--- src/test/java/br/com/alura/loja/LojaApplicationTests.java
+++ src/test/java/br/com/alura/loja/LojaApplicationTests.java
@@ -1,13 +1,6 @@
 package br.com.alura.loja;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class LojaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
